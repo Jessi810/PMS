7e4626ecbb785ea83f663db416e482b6c441f4b6
Jessi <jessisibayan@gmail.com>
2016-11-24 22:51:27 +0800

    Fixed ListItem not clickable

--- app/src/main/java/com/jessi/pms/PatientList.java
+++ app/src/main/java/com/jessi/pms/PatientList.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -58,6 +60,12 @@ public class PatientList extends AppCompatActivity {
         // Attach the adapter to a ListView
         ListView listView = (ListView) findViewById(R.id.patients_listview);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         database.child("Patients").addValueEventListener(new ValueEventListener() {
             @Override
